d4f870f29481c48c1bddadd11bc89272e4fd27ce
AEren46 <77582721+AEren46@users.noreply.github.com>
2021-01-19 18:58:50 +0300

    Deneme

---
+++ MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri mx) 
+        {
+            Console.WriteLine("Yeni üyemiz hayırlı olsun:");
+            Console.WriteLine("Ad:     "+ mx.Ad);
+            Console.WriteLine("Soyad:  "+ mx.Soyad);
+        }
+
+        public void Cikar(Musteri my) 
+        {
+            Console.WriteLine("Güle güle! Bizi kaybeden kişi:    ");
+            Console.WriteLine("Ad:     " + my.Ad);
+            Console.WriteLine("Soyad:  " + my.Soyad);
+        }
+
+        public void Listele(Musteri[] liste) 
+        {
+            foreach (Musteri mus in liste)
+            {
+                Console.WriteLine("Müşteri Id     :    " + mus.Id);
+                Console.WriteLine("Müşteri Adı    :    " + mus.Ad);
+                Console.WriteLine("Müşteri Soyadı :    " + mus.Soyad);
+                Console.WriteLine("Müşteri Yaşı   :    " + mus.Yas);
+                Console.WriteLine("Müşteri Parası :    " + mus.Para);
+                Console.WriteLine("----------------------------");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri(), m2 = new Musteri(), m3 = new Musteri();
+            Musteri[] musteriler = new Musteri[] { m1, m2, m3 };
+            m1.Id = "123qwe";
+            m1.Ad = "Ahmet";
+            m1.Soyad = "Toprak";
+            m1.Yas = 23;
+            m1.Para = 12000;
+            m2.Id = "456asd";
+            m2.Ad = "Mehmet";
+            m2.Soyad = "Elmas";
+            m2.Yas = 46;
+            m2.Para = 29600;
+            m3.Id = "789zxc";
+            m3.Ad = "Sakıp";
+            m3.Soyad = "Tarlacı";
+            m3.Yas = 37;
+            m3.Para = 357000;
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(m1);
+            musterimanager.Ekle(m2);
+            musterimanager.Ekle(m3);
+            Console.WriteLine("==============");
+            musterimanager.Listele(musteriler);
+            musterimanager.Cikar(m2);
+            musterimanager.Cikar(m3);
+
+
+        }
+    }
+}
